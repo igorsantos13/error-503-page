09ac91763b2f763aba627c6194f3b9111f1a412b
Igor Santos <igorsantos9513@outlook.com>
2023-06-30 17:55:10 -0300

    hero section changed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Image from 'next/image'
-import hero from './503 Error Service Unavailable-amico.svg'
+import hero from './503 Error Service Unavailable-bro.svg'
 
 type Props = {}
 
 export default function Hero({}: Props) {
   return (
-    <Image src={hero} alt='hero'></Image>
+    <div className='flex flex-col items-center justify-center w-screen h-screen'>
+      <Image src={hero} alt='hero' className='bounce-animation'></Image>
+      <h3 className='font-bold text-purple-700'>oops, i think we unplugged the wrong cable here...</h3>
+      <p className='text-white'>No worries! We're trying our best to fix it asap!</p>
+    </div>
   )
 }
